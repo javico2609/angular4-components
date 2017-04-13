457a0145f739ab4225def5c8f1bb15b33025d30d
INFODOM\javierg <javierg@infocorp.com.uy>
2017-04-13 12:58:55 -0300

    change nabvar component

--- src/app/wizard/wizard-navbar/wizard-navbar.component.ts
+++ src/app/wizard/wizard-navbar/wizard-navbar.component.ts
@@ -9,13 +9,22 @@ import { WizardStepComponent } from "../wizard-step/wizard-step.component";
 export class WizardNavbarComponent implements OnInit {
 
   private _steps: WizardStepComponent[];
-  
+  private _currentStepIndex: number = 0;
+
   @Input()
   set steps(steps: WizardStepComponent[]) {
     this._steps = steps;
   }
   get steps() {
     return this._steps;
+  }
+
+  @Input()
+  set currentStepIndex(currentStepIndex: number) {
+    this._currentStepIndex = currentStepIndex;
+  }
+  get currentStepIndex() {
+    return this._currentStepIndex;
   }
 
   constructor() { }
